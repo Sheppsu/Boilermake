af754481fbd261b5aaf020392eb8432e7a207dcf
GuardianGuy <dgedgaud@gmail.com>
2025-02-22 21:04:14 -0500

    Update settings.h

--- KappaPad_Chuni/settings.h
+++ KappaPad_Chuni/settings.h
@@ -43,25 +43,41 @@ void settingsMode() {
   int stillSetting = 1;
   while (stillSetting) {
     while(!Serial.available()){}
+    //commands should be formatted as <command>.<target>.<value> where value is an ascii character
+    //<command> can be any of the strings in the if(command == "<string>") blocks below
+    //<target> must be which keypad key is being changed (left, right, k1, k2, k3, k4) for both the 'key' and 'mod' commands
+    //<value> is specified in it's command's if() block, not needed for save or exit
     command = Serial.readStringUntil('.');
     target = Serial.readStringUntil('.');
     if (command == "key") {
-      //only expects a single byte because I'm tired right now
+      //only expects liteally just the letter or number the button is being set to because I'm tired right now
       //0x04 - 0x27 are letters and then numbers, can be sent and read as single char
-      //only implementing function keys 0xc2 - 0xfb for F1-F24
+      //probably also works all the rest of the normal 
       EEPROM.write(targetToCode(target), Serial.read());
     }
+    else if (command == "fkey") {
+      //function keys 0xc2 - 0xfb (194-251) for F1-F24
+      //expects input of the entire number as a base 10 integer
+      EEPROM.write(targetToCode(target), Serial.parseInt(SKIP_ALL));
+    }
     else if (command == "mod") {
-      EEPROM.write(targetToCode(target) + 7, Serial.read());
+      //expects values from 0 to 7 corresponding to three bits 0bABC
+      //the A bit controls the shift key
+      //the B bit controls the ctrl key
+      //the C bit controls the alt key
+      EEPROM.write(targetToCode(target) + 7, Serial.parseInt(SKIP_ALL));
     }
     else if (command == "thresh") {
-      EEPROM.write(6, Serial.read());
+      //give it an integer 0 to 255
+      EEPROM.write(6, Serial.parseInt(SKIP_ALL));
     }
     else if (command == "save") {
+      //<target> should probably not be blank, I suggest the string "settings"
       EEPROM.commit();
       delay(1000);
     }
     else if (command == "exit") {
+      //same comment as save command
       Serial.print("see you next time");
       Serial.flush();
       stillSetting = 0;
